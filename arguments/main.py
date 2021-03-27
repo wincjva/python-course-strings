ad5383bfa791482f48e563028fa69cfbd6c5a28e
wincjva <81095795+wincjva@users.noreply.github.com>
2021-03-27 14:44:52 +0100

    Update main.py

--- arguments/main.py
+++ arguments/main.py
@@ -3,6 +3,8 @@ __winc_id__ = "7b9401ad7f544be2a23321292dd61cb6"
 __human_name__ = "arguments"
 
 # Add your code after this line
+
+
 def greet(name, template="Hello, <name>!"):
     return template.replace("<name>", name)
 
